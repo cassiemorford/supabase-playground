1fb9dbdcb405903a00c8fb3e16b9630faeb3d6a9
cassie morford <cassiemorford@gmail.com>
2024-11-24 12:21:13 -0800

    small styling changes to wrapper

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,13 +27,13 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="min-h-screen flex flex-col items-center h-screen">
-            <div className="flex-1 w-full flex flex-col p-10 items-center">
-              <div className="relative w-10/12 max-w-5x h-5/6">
-                <div className="absolute -inset-2 rounded-lg bg-gradient-to-r from-indigo-600 via-emerald-600 to-sky-600 opacity-75 blur"></div>
+          <main className="min-h-screen h-screen bg-slate-900">
+            <div className="flex-1 w-full h-full flex flex-col py-16 items-center">
+              <div className="relative w-10/12 max-w-5x h-full">
+                <div className="absolute -inset-5 rounded-lg bg-gradient-to-br from-indigo-600 via-emerald-600 to-sky-600 opacity-75 blur"></div>
                 <div className="relative flex-col items-center justify-center rounded-lg bg-slate-900 h-full overflow-y-hidden">
                   <div className="flex flex-col gap-6 justify-between w-full">
-                    <nav className="flex gap-20 p-5 justify-between border-b-4 text-slate-200 border-emerald-950 z-10 bg-slate-900">
+                    <nav className="flex gap-20 p-5 justify-between border-b-4 text-slate-200 border-emerald-950 z-10 bg-emerald-900">
                       <span className="text-2xl">Cassie Morford</span>
                       <ul className="flex gap-10 align-middle ">
                         <Link href="/">Home</Link>
--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -6,5 +6,12 @@ export default async function TodoItems() {
 
   const { data: todoListItems } = await supabase.from("todo_items").select();
 
-  return <TodoList todoListItems={todoListItems || []} />;
+  return (
+    <div className="text-sky-300 p-4">
+      <h1>
+        practicing using supabase to build a simple todo list with live updates
+      </h1>
+      <TodoList todoListItems={todoListItems || []} />
+    </div>
+  );
 }
